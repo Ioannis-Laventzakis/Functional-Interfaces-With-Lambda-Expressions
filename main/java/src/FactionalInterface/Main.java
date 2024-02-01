4ea372ed49d8cdfd4c500325f2ae33b4a4d270fe
Ioannis84 <glaventzakis@gmail.com>
2024-02-01 11:54:00 +0100

    new file

--- main/java/src/FactionalInterface/Main.java
+++ main/java/src/FactionalInterface/Main.java
@@ -2,17 +2,33 @@ package FactionalInterface;
 
 public class Main {
     public static void main(String[] args) {
+        // Define an array of integers to be processed.
         int[] numbers = {1, 2, 3, 4, 5};
 
+        // Create a NumberProcessor instance using a lambda expression to double a number.
         NumberProcessor doubler = number -> number * 2;
+
+        // Create a NumberProcessor instance using a lambda expression to square a number.
         NumberProcessor squarer = number -> number * number;
+
+        // Create a NumberProcessor instance using a lambda expression to increment a number by one.
         NumberProcessor incrementer = number -> number + 1;
 
+        // Loop through each number in the array.
         for (int number : numbers) {
+            // Print the original number.
             System.out.println("Original: " + number);
+
+            // Print the result of doubling the number.
             System.out.println("Doubled: " + doubler.process(number));
+
+            // Print the result of squaring the number.
             System.out.println("Squared: " + squarer.process(number));
+
+            // Print the result of incrementing the number.
             System.out.println("Incremented: " + incrementer.process(number));
+
+            // Print a blank line for readability.
             System.out.println();
         }
     }
--- main/java/src/FactionalInterface/NumberProcessor.java
+++ main/java/src/FactionalInterface/NumberProcessor.java
@@ -1,10 +1,10 @@
 package FactionalInterface;
 
+// This is a functional interface named NumberProcessor.
+// Functional interfaces have only one abstract method.
 @FunctionalInterface
 public interface NumberProcessor {
+    // This is the abstract method of the interface.
+    // It takes an integer as input and returns an integer.
     int process(int number);
-
-    NumberProcessor doubler = number -> number * 2;
-NumberProcessor squarer = number -> number * number;
-NumberProcessor incrementer = number -> number + 1;
 }
