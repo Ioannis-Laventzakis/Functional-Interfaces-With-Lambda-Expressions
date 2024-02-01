ff8e8e1d407891b0ca2503e5f053bd2a0116a799
Ioannis84 <glaventzakis@gmail.com>
2024-02-01 12:37:55 +0100

    new file

--- main/java/src/FactionalInterface/NumberProcessor.java
+++ main/java/src/FactionalInterface/NumberProcessor.java
@@ -1,10 +1,12 @@
 package FactionalInterface;
 
 // This is a functional interface named NumberProcessor.
+
 // Functional interfaces have only one abstract method.
 @FunctionalInterface
 public interface NumberProcessor {
     // This is the abstract method of the interface.
+
     // It takes an integer as input and returns an integer.
     int process(int number);
 }
